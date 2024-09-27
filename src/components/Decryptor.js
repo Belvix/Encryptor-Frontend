04f72a44b6db4926489c7dade8af9596ad8c9a0f
Srichaitanya C <csrichaitanya2003@gmail.com>
2024-09-28 00:38:00 +0530

    .

--- src/components/Decryptor.js
+++ src/components/Decryptor.js
@@ -5,14 +5,12 @@ import './styles.css';
 const Decryptor = () => {
   const canvasRef = useRef(null);
 
-  // Handle clearing the canvas
   const handleClearCanvas = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   };
 
-  // Handle saving the drawing
   const handleSaveCanvas = () => {
     const canvas = canvasRef.current;
     const dataURL = canvas.toDataURL('image/png');
@@ -24,12 +22,10 @@ const Decryptor = () => {
 
   return (
     <div className="page-container">
-      {/* Go Back Button */}
       <Link to="/" className="go-back-button">{'<'} Go Back</Link>
 
       <h1>Decryptor</h1>
 
-      {/* Canvas for drawing */}
       <div className="canvas-container">
         <canvas ref={canvasRef} width={800} height={400} className="canvas" />
         <button onClick={handleClearCanvas} className="canvas-clear-button">
@@ -40,22 +36,18 @@ const Decryptor = () => {
         </button>
       </div>
 
-      {/* File upload section */}
       <div id="uploadedFileArea">
         <h4>Upload File</h4>
         <input type="file" />
       </div>
 
-      {/* Decrypt Button */}
       <button>Decrypt</button>
 
-      {/* Decrypted File section */}
       <div id="decryptedFileArea">
         <h4>Decrypted File</h4>
         <input type="file" />
       </div>
 
-      {/* Download Decrypted File Button */}
       <button>Download Decrypted File</button>
     </div>
   );
--- src/components/Encryptor.js
+++ src/components/Encryptor.js
@@ -5,14 +5,12 @@ import './styles.css';
 const Encryptor = () => {
   const canvasRef = useRef(null);
 
-  // Handle clearing the canvas
   const handleClearCanvas = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   };
 
-  // Handle saving the drawing
   const handleSaveCanvas = () => {
     const canvas = canvasRef.current;
     const dataURL = canvas.toDataURL('image/png');
@@ -24,12 +22,10 @@ const Encryptor = () => {
 
   return (
     <div className="page-container">
-      {/* Go Back Button */}
       <Link to="/" className="go-back-button">{'<'} Go Back</Link>
 
       <h1>Encryptor</h1>
 
-      {/* Canvas for drawing */}
       <div className="canvas-container">
         <canvas ref={canvasRef} width={800} height={400} className="canvas" />
         <button onClick={handleClearCanvas} className="canvas-clear-button">
@@ -40,22 +36,18 @@ const Encryptor = () => {
         </button>
       </div>
 
-      {/* File upload section */}
       <div id="uploadedFileArea">
         <h4>Upload File</h4>
         <input type="file" />
       </div>
 
-      {/* Encrypt Button */}
       <button>Encrypt</button>
 
-      {/* Encrypted File section */}
       <div id="encryptedFileArea">
         <h4>Encrypted File</h4>
         <input type="file" />
       </div>
 
-      {/* Download Encrypted File Button */}
       <button>Download Encrypted File</button>
     </div>
   );
