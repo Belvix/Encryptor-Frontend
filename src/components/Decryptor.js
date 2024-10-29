533bb95bdd62a5e79a91f064ddb1d1118d629b2f
Srichaitanya C <csrichaitanya2003@gmail.com>
2024-10-29 22:33:50 +0530

    .

--- src/components/Decryptor.js
+++ src/components/Decryptor.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef , useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
 import './CanvasComponent'
@@ -6,6 +6,21 @@ import CanvasComponent from './CanvasComponent';
 
 const Decryptor = () => {
   const canvasRef = useRef(null);
+  const [fileName, setFileName] = useState('No file chosen');
+  const [uploadSuccess, setUploadSuccess] = useState(false);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+      setUploadSuccess(true);
+      setTimeout(() => {
+        setFileName('No file chosen');
+        setUploadSuccess(false);
+        e.target.value = '';
+      }, 200000);
+    }
+  };
   return (
     <div className="page-container">
       <Link to="/" className="go-back-button">Go Back</Link>
@@ -16,9 +31,20 @@ const Decryptor = () => {
         <CanvasComponent ref={canvasRef} width={800} height={400} className="canvas" />
       </div>
 
-      <div id="uploadedFileArea">
+      <div id="uploadedFileArea" className={`upload-area ${uploadSuccess ? 'upload-success' : ''}`}>
         <h4>Upload File</h4>
-        <input type="file" />
+        {!uploadSuccess && (
+          <>
+            <label htmlFor="file-upload" className="file-upload-label">Choose a file</label>
+            <input type="file" id="file-upload" onChange={handleFileChange} />
+          </>
+        )}
+        {uploadSuccess && (
+          <div className="uploaded-file-info">
+            <p className="file-name">{fileName}</p>
+            <p className="success-message">File Uploaded!</p>
+          </div>
+        )}
       </div>
 
       <button>Decrypt</button>
--- src/components/Encryptor.js
+++ src/components/Encryptor.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef , useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
 import './CanvasComponent'
@@ -6,6 +6,22 @@ import CanvasComponent from './CanvasComponent';
 
 const Encryptor = () => {
   const canvasRef = useRef(null);
+  const [fileName, setFileName] = useState('No file chosen');
+  const [uploadSuccess, setUploadSuccess] = useState(false);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+      setUploadSuccess(true);
+      setTimeout(() => {
+        setFileName('No file chosen');
+        setUploadSuccess(false);
+        e.target.value = '';
+      }, 200000);
+    }
+  };
+
   return (
     <div className="page-container">
       <Link to="/" className="go-back-button">Go Back</Link>
@@ -16,11 +32,22 @@ const Encryptor = () => {
         <CanvasComponent ref={canvasRef} width={800} height={400} className="canvas" />
       </div>
 
-      <div id="uploadedFileArea">
+      <div id="uploadedFileArea" className={`upload-area ${uploadSuccess ? 'upload-success' : ''}`}>
         <h4>Upload File</h4>
-        <input type="file" />
+        {!uploadSuccess && (
+          <>
+            <label htmlFor="file-upload" className="file-upload-label">Choose a file</label>
+            <input type="file" id="file-upload" onChange={handleFileChange} />
+          </>
+        )}
+        {uploadSuccess && (
+          <div className="uploaded-file-info">
+            <p className="file-name">{fileName}</p>
+            <p className="success-message">File Uploaded!</p>
+          </div>
+        )}
       </div>
-
+      
       <button>Encrypt</button>
 
       <div id="encryptedFileArea">
